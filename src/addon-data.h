b66d0c3a50ec4c3407e120c05121a049326dd7d1
Andrew Dupont <github@andrewdupont.net>
2024-10-07 16:59:37 -0700

    (oops)

--- src/addon-data.h
+++ src/addon-data.h
@@ -1,4 +1,11 @@
 #include "common.h"
 #pragma once
 
-// TODO: Remove
+class AddonData final {
+public:
+  explicit AddonData(Napi::Env env) {}
+
+  Napi::FunctionReference callback;
+  PathWatcherWorker* worker;
+  int watch_count;
+};
