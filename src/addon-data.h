8f1cf55be14151bdeea20db669e954e649a3fa86
Andrew Dupont <github@andrewdupont.net>
2024-10-09 11:00:40 -0700

    Remove Windows `AddonData` stuff

--- src/addon-data.h
+++ src/addon-data.h
@@ -14,18 +14,7 @@ public:
   int kqueue;
   int init_errno;
 #endif
-  // Not macOS.
-#ifdef _WIN32
-  // Mutex for the HandleWrapper map.
-  uv_mutex_t handle_wrap_map_mutex;
-  // The events to be waited on.
-  std::vector<HANDLE> events;
-  // The dummy event to wakeup the thread.
-  HANDLE wake_up_event;
-  // The dummy event to ensure we are not waiting on a file handle when
-  // destroying it.
-  HANDLE file_handles_free_event;
-#endif
+
 #ifdef __linux__
   // Linux.
   int inotify;
