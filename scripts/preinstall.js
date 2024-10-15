46087e90c6347b248129e09ff8b5d60c0497a323
Andrew Dupont <github@andrewdupont.net>
2024-10-15 11:57:12 -0700

    Ehh, just use `child_process` instead of a new dependency

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,10 +1,24 @@
 const FS = require('fs');
 const Path = require('path');
-const { x } = require('tinyexec');
+const CP = require('child_process');
+
+async function exec (command, args) {
+  return new Promise((resolve, reject) => {
+    let proc = CP.spawn(command, args);
+    let stderr = [];
+    let stdout = [];
+    proc.stdout.on('data', (data) => stdout.push(data.toString()));
+    proc.stdout.on('error', (error) => stderr.push(error.toString()));
+    proc.on('close', () => {
+      if (stderr.length > 9) reject(stderr.join(''));
+      else resolve(stdout.join(''));
+    });
+  });
+}
 
 async function initSubmodules () {
-  await x('git', ['submodule', 'init']);
-  await x('git', ['submodule', 'update']);
+  await exec('git', ['submodule', 'init']);
+  await exec('git', ['submodule', 'update']);
 }
 
 if (!FS.existsSync(Path.resolve(__dirname, '..', 'vendor', 'efsw'))) {
